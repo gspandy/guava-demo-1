4549bd9b16dbb98b72751bd542ea63195dda94e6
aaron <aaron.pritzlaff@bjss.com>
2011-06-15 12:13:32 +0100

    test data added to galaxy spec

--- src/test/scala/demo/guava/GalaxySpec.scala
+++ src/test/scala/demo/guava/GalaxySpec.scala
@@ -1,13 +1,13 @@
 package com.porpoise
 
 import scala.collection.JavaConversions._
-
-import demo.guava.GalaxyMother
+import demo.guava._
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.{ FeatureSpec, FlatSpec, FunSuite, Spec }
 import org.junit.Assert
+import demo.guava.Galaxy
 
 /**
  * Specification for a Galaxy
@@ -15,9 +15,25 @@ import org.junit.Assert
 @RunWith(classOf[JUnitRunner])
 class ScalaBeanSpec extends Spec with ShouldMatchers {
 
+  def newTestData = new {
+    val galaxy = new Galaxy()
+    def newSystem = {
+      def newPlanet(name: String) = (new Planet(name, name.length))
+      val system = new PlanetarySystem()
+      system.addPlanet(newPlanet("Earth"))
+      system.addPlanet(newPlanet("Venus"))
+      system.addPlanet(newPlanet("Mars"))
+      system.addPlanet(newPlanet("Saturn"))
+      system
+    }
+    galaxy.addSystem(newSystem)
+    galaxy.addSystem(newSystem)
+  }
+
   describe("A Galaxy") {
     it("should be able to get all planets") {
-      val galaxy = GalaxyMother.create()
+      val data = newTestData
+      import data._
       val planets = galaxy.getPlanets
 
       Assert.assertFalse(galaxy.getSystems.isEmpty)
