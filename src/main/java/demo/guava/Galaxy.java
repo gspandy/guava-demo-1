f61e84b2452871dafa2885973f384d8079d5f928
aaron <aaron.pritzlaff@bjss.com>
2011-06-15 10:49:45 +0100

    XXX functions removed

--- src/main/java/demo/guava/Galaxy.java
+++ src/main/java/demo/guava/Galaxy.java
@@ -6,8 +6,8 @@ import java.util.Map;
 import com.google.common.collect.Lists;
 import com.porpoise.common.collect.Sequences;
 
-import demo.guava.functions.PlanetFunctions;
-import demo.guava.functions.PlanetarySystemFunctions;
+import demo.guava.metadata.PlanetAccessors;
+import demo.guava.metadata.PlanetarySystemAccessors;
 
 /**
  */
@@ -39,20 +39,20 @@ public class Galaxy {
      */
     public Collection<Planet> getPlanets() {
         return Sequences.flatMap(getSystems(),
-                PlanetarySystemFunctions.GET_PLANETS);
+                PlanetarySystemAccessors.GET_PLANETS);
     }
 
     /**
      * @return all the planets in this galaxy
      */
     public Map<String, Collection<Planet>> getPlanetsByName() {
-        return Sequences.groupBy(getPlanets(), PlanetFunctions.GET_NAME);
+        return Sequences.groupBy(getPlanets(), PlanetAccessors.GET_NAME);
     }
 
     /**
      * @return all the planets in this galaxy
      */
     public Map<String, Planet> getPlanetsByNameUnique() {
-        return Sequences.groupByUnique(getPlanets(), PlanetFunctions.GET_NAME);
+        return Sequences.groupByUnique(getPlanets(), PlanetAccessors.GET_NAME);
     }
 }
